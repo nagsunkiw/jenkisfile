f7fda914ec2e6e941fc063d1e430cff5b52793d3
Nagaraj Kiwde <nagarajkiwde12@gmail.com>
2022-08-27 12:07:31 +0530

    adding sh.py

---
+++ sh.py
@@ -0,0 +1,35 @@
+"""a=10
+if a==10:
+    print(a)"""
+
+
+"""a=int(input("Enter number:"))
+if a==10:
+    print(a)
+else:
+    print("value of a:",a)"""
+
+"""a=int(input("Enter number:"))
+if a>10:
+     print("first if condition:",a)
+     if a<20:
+        print("nested if condition:",a)
+
+     else:
+            print("Nested else block:",a)
+else:
+    print("last else block:",a)"""
+
+"""a=int(input("Enter number:"))
+if a>10:
+    print("first condition:",a)
+elif a<15:
+        print("elif condition:",a)
+else:
+    print("Both conditions are false:",a)"""
+
+"""a=10
+if a==10: print(a)"""
+
+a=10
+print(a) if a==10 else print("else block")
